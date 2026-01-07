41ab0b3d9820b67184989f6b50c9942c99977a0c
Tanjimkabir <rakibulkabirkhan03@example.com>
2026-01-08 00:04:59 +0600

    Added Registrationcheck Page

--- Controllers/Registrationcheck.php
+++ Controllers/Registrationcheck.php
@@ -1,5 +1,5 @@
 <?php
-    //Rakibul Kabir Khan
+    //Rakibul Kabir Khan Registrationcheck Page
     session_start();
     require_once('../Models/LoginandregistrationModel.php');
     if(isset($_POST['submit'])){
