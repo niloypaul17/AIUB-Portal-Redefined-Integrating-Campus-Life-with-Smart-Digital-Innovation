45d5e52b7b5114493bcbef9fddd8021119f58f5f
jain160 <mdaljabil159@gmail.com>
2026-01-08 02:20:55 +0600

    Universityareasystem.php

---
+++ Views/Universityareasystem.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <title>University Campus Map</title>
+    <style>
+        body {
+            background-color: skyblue;
+        }
+        h3 {
+            text-align: center;
+        }
+        img.logo {
+            display: block;
+            margin-top: 100px;
+            margin-left: 900px;
+        }
+        .box {
+            width: 900px;
+            margin: auto;
+            padding: 20px;
+            background-color: white;
+            border: 2px solid black;
+            text-align: center;
+        }
+        img.map {
+            width: 100%;
+            border: 1px solid black;
+        }
+        a {
+            display: block;
+            text-align: center;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+
+<body>
+
+<img src="AIUB Logo.png" width="100" class="logo">
+<h3>University Campus Map</h3>
+
+<div class="box">
+    <p><b>AIUB Campus Map</b></p>
+    <br>
+    <img src="AIUB Map.PNG" class="map" alt="AIUB Campus Map">
+</div>
+
+<a href="Dashboard.php">Back</a>
+
+</body>
+</html>
