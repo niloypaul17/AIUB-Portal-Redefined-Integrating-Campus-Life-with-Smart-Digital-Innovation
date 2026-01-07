0270c208aafe8be93bf763d5bf5e1b6479b23102
Niloy Paul <niloypaul38244@gmail.com>
2026-01-08 02:29:51 +0600

    Student_ID_reacquire_application_system_check

--- Models/Student_ID_reacquire_application_system_check.php
+++ Models/Student_ID_reacquire_application_system_check.php
@@ -1,10 +1,9 @@
 <?php
-//Student_ID_reacquire_application_system_check
 require_once('Database.php');
 
 function studentcheck($student){
     $con = getConnection();
-    $sql = "INSERT INTO student_id_application(department,time) VALUES({$student['department']}, {$student['time']})";
+    $sql = "INSERT INTO student_id_application(department,time) VALUES('{$student['department']}', '{$student['time']}')";
     $result = mysqli_query($con, $sql);   
     
     if($result){
