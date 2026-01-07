451d9c5e634681f390301f2b5737c31abefeec3c
Tanjimkabir <rakibulkabirkhan03@example.com>
2026-01-08 00:26:50 +0600

    Added uploadpicture Page

--- Controllers/uploadpicture.php
+++ Controllers/uploadpicture.php
@@ -1,5 +1,5 @@
 <?php
-    //Rakibul Kabir Khan uploadpicture Page
+    //uploadpicture Page
     $src = $_FILES['lostitempicture']['tmp_name'];
     $ext = explode('.', $_FILES['lostitempicture']['name']);
     $count = count($ext);
