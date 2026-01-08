45cb5f456ddff63fa3456fd2226ab1747986c707
jain160 <mdaljabil159@gmail.com>
2026-01-08 10:34:06 +0600

    Gymnasiumregistrationform

--- Views/Gymnasiumregistrationform.php
+++ Views/Gymnasiumregistrationform.php
@@ -42,7 +42,7 @@
 </head>
 
 <body>
-<img src="AIUB Logo.png" width="100">
+<img src="../Assests/AIUB_Logo.png" width="100">
 <h3>Gymnasium Registration Form</h3>
 
 <form method="POST" action="Gymnasiumregistrationformcheck.php" enctype="multipart/form-data">
