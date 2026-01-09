9e1911e526ee3c64bb951051e81d0ae881fa384d
Niloy Paul <niloypaul38244@gmail.com>
2026-01-10 03:35:21 +0600

    JS Validation

--- Controllers/Student_ID_reacquire_application_system_Valid.php
+++ Controllers/Student_ID_reacquire_application_system_Valid.php
@@ -1,6 +1,4 @@
 <?php
-//Student_ID_reacquire_application_system_Valid
-
 require_once("Authenticationcheck.php");
 require_once("../Models/Student_ID_reacquire_application_system_check.php");
 
@@ -8,21 +6,22 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
     $department=$_POST["department"];
     $time=$_POST["date"];
+    $massage2="";
 
     if($department=="" && empty($time)){
-        $massage="Fill all boxs";
+        $massage="Fill all boxs from php";
     }
     else if($department=="" ){
-        $massage="Fill department name";
+        $massage="Fill department name from php";
     }
     else if(empty($time)){
-        $massage="Fill time";
+        $massage="Fill time from php";
     }
     else{
         $student=['department'=>$department,'time'=>$time];
         $status=studentcheck($student);
         if($status){
-            $massage2="successfully submitted! TIME: ".$time;
+            $massage2="successfully submitted! TIME OF LOST: ".$time;
         }
     }
 }
