94683b46dedb9af225c7ef1a439537314f5be97b
Niloy Paul <niloypaul38244@gmail.com>
2026-01-10 03:34:53 +0600

    JS Validation

--- Controllers/Course_Book_Reference_and_Online_Resources_Valid.php
+++ Controllers/Course_Book_Reference_and_Online_Resources_Valid.php
@@ -4,15 +4,19 @@ require_once("../Models/Course_Book_Reference_check.php");
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     $course=$_POST["course"];
+    $massage2="";
     
     if($course==""){
-        $massage="Fill course name";
+        $massage="Fill course name from php";
     }
     else{
         $course=['coursename'=>$course];
         $status=coursecheck($course);
         if($status){
-            $massage2="successful";
+            $massage2='<p style="text-decoration: solid; color:blue">Book website here</p>';
+        }
+        else{
+            $massage2="unsuccessful";
         }
     }
 }
