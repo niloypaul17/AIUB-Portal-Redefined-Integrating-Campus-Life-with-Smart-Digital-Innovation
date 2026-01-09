54c9d77a42aacfbb508e78a9cbad633eec44cdc2
Niloy Paul <niloypaul38244@gmail.com>
2026-01-10 03:34:18 +0600

    JS Validation

--- Controllers/Indoor_Games_playing_slot_Valid.php
+++ Controllers/Indoor_Games_playing_slot_Valid.php
@@ -5,9 +5,10 @@ require_once("../Models/Indoor_Games_playing_slot_check.php");
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     $gamename=$_POST["S1"];
     $time=$_POST["S2"];
+    $massage2="";
     
     if($gamename=="" && $time==""){
-        $massage="Fill every box";
+        $massage="Fill every box from php";
     }
     else{
         $game=['gamename'=>$gamename,'time'=>$time];
