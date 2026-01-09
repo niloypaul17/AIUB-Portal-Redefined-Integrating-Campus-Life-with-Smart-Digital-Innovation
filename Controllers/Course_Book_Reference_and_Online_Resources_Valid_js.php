7dafc3243fe572ac8a0441f00bded631ef574bb9
Niloy Paul <niloypaul38244@gmail.com>
2026-01-10 03:29:59 +0600

    JS Validation

--- Controllers/Course_Book_Reference_and_Online_Resources_Valid_js.php
+++ Controllers/Course_Book_Reference_and_Online_Resources_Valid_js.php
@@ -1,3 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <script>
+        function checkbook(){
+            let course=document.getElementById("I1").value;
+            let div=document.getElementById("c1");
 
-?>
+            if(course==""){
+                div.innerHTML="Fill course name from js";
+                return true;
+            }
+            else{
+                div.innerHTML="successful from js";
+                return true;
+            }
+            return true;
+        }
+    </script>
+</html>
