74744acd2725795b4138e5a514fff37d254fc883
Niloy Paul <niloypaul38244@gmail.com>
2026-01-10 03:27:20 +0600

    JS Validation

--- Controllers/Student_ID_reacquire_application_system_Valid_js.php
+++ Controllers/Student_ID_reacquire_application_system_Valid_js.php
@@ -1,5 +1,28 @@
-<?php
-//Student_ID_reacquire_application_system_Valid_js
-//Student_ID_reacquire_application_system_Valid_js
+<!DOCTYPE html>
+<html>
+    <script>
+        function checkstudent(){
+            let department=document.getElementById("I1").value;
+            let time=document.getElementById("I2").value;
+            let div=document.getElementById("c1");
 
-?>
+            if(department=="" && !time.value){
+                div.innerHTML="Fill all boxs from js";
+                return true;
+            }
+            else if(department=="" ){
+                div.innerHTML="Fill department name from js";
+                return true;
+            }
+            else if(!time.value){
+                div.innerHTML="Fill time from js";
+                return true;
+            }
+            else{
+                div.innerHTML="Successful from js";
+                return true;
+            }
+            return true;
+        }
+    </script>
+</html>
