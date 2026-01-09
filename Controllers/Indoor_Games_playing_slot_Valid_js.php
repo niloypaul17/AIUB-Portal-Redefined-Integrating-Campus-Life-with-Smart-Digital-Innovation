e9cf013b342549276d552d6d3ed8d9d2dc337573
Niloy Paul <niloypaul38244@gmail.com>
2026-01-10 03:28:18 +0600

    JS Validation

--- Controllers/Indoor_Games_playing_slot_Valid_js.php
+++ Controllers/Indoor_Games_playing_slot_Valid_js.php
@@ -1,2 +1,20 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+    <script>
+        function checkgame(){
+            let gamename= document.getElementById("I2").value;
+            let time=document.getElementById("I3").value;
+            let div=document.getElementById("c1");
+
+            if(gamename=="" && time==""){
+                div.innerHTML="Fill every box from js";
+                return true;
+            }
+            else{
+                div.innerHTML="Successfull";
+                return true;
+            }
+            return true;
+        }
+    </script>
+</html>
