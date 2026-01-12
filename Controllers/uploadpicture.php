0b7b5dd92fba8b29e3de1d638577e77bede66488
Niloy Paul <niloypaul38244@gmail.com>
2026-01-12 11:12:50 +0600

    Upload File

--- Controllers/uploadpicture.php
+++ Controllers/uploadpicture.php
@@ -4,7 +4,7 @@
     $count = count($ext);
     $fileExt = $ext[$count-1];
     $originalName = $ext[0];
-    $des = "../Upload/".$originalName."_".time().".".$fileExt;
+    $des = "../Assests/Upload/".$originalName."_".time().".".$fileExt;
 
    if(move_uploaded_file($src, $des)){
     echo "success";
