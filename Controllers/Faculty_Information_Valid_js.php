646b5f4c7748c643201c3fb4c9173a27dcb17235
Niloy Paul <niloypaul38244@gmail.com>
2026-01-12 08:39:07 +0600

    AJAX & JASON

--- Controllers/Faculty_Information_Valid_js.php
+++ Controllers/Faculty_Information_Valid_js.php
@@ -1,15 +1,26 @@
-<!DOCTYPE html>
-<html>
-    
-<script>
-        function faculty(){
-            let fid=document.getElementById("Input1");
-            let div=document.getElementById("c1");
 
-           if(fid==""){
-               div.innerHTML="Fill name from js";
-               return true;
+<script>
+    function ajaxapply(){
+        let fname=document.getElementById("Input1").value;
+
+        let faculty1 ={
+            'name': fname
+        };
+
+        let data=JSON.stringify(faculty1);
+        let xhttp = new XMLHttpRequest();
+        xhttp.open('POST', '../Models/datafaculty.php', true);
+        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhttp.send('faculty='+data);
+        xhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                let userData = JSON.parse(this.responseText);
+                document.getElementById('c1').innerHTML = `
+                Name: ${userData.facultyname}<br><br>
+                Position: ${userData.position}<br><br>
+                Email: ${userData.email}<br><br>
+                Room Number: ${userData.location}<br><br>`;
             }
-           return true;
-      }
+        }
+    }
 </script>
