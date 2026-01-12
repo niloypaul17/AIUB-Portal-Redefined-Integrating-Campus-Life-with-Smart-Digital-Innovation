77c3f467e229626178b86937824bb9c0164baf0e
Niloy Paul <niloypaul38244@gmail.com>
2026-01-12 08:40:27 +0600

    AJAX & JASON

--- Controllers/Faculty_Information_Valid.php
+++ Controllers/Faculty_Information_Valid.php
@@ -1,7 +1,6 @@
 <?php
 require_once("Authenticationcheck.php");
-require_once("../Models/faculty_information_check.php");
-
+require_once("../Models/datafaculty.php");
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     $fname=$_POST["fid"];
     $massage2="";
@@ -10,14 +9,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         $massage="Fill name from php";
     }
     else{
-        $faculty=['facultyname'=>$fname];
-        $r=facultycheck($faculty);
-        if($r){
-        $massage2=$r;
-        }
-        else{
-            $massage2="Invalid Faculty name";
-        }
+        $_SESSION['fa']=$fname;
     }
 }
 ?>
